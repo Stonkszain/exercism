1c16ffb4b0027722eb896200ec661ec7c3393ca1
Stonkszain <85150052+Stonkszain@users.noreply.github.com>
2023-08-07 09:42:24 +0100

    Create Darts.py

---
+++ python/Darts.py
@@ -0,0 +1,12 @@
+from math import sqrt
+
+def score(x, y):
+    dist = sqrt(x**2 + y**2)
+    if dist <= 1:
+        return 10
+    elif dist <= 5:
+        return 5
+    elif dist <= 10:
+        return 1
+    return 0
+    # pass
