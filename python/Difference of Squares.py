7332f795cba102eb10c09c12d3f606cade8497a6
Stonkszain <85150052+Stonkszain@users.noreply.github.com>
2023-08-07 09:32:32 +0100

    Create Difference of Squares.py

---
+++ python/Difference of Squares.py
@@ -0,0 +1,19 @@
+def square_of_sum(number):
+    sum = 0
+    for i in range( number ):
+        sum += (i + 1)
+    return sum ** 2
+    # pass
+
+
+def sum_of_squares(number):
+    sum = 0
+    for i in range( number ):
+        sum += (i + 1) ** 2
+    return sum
+    # pass
+
+
+def difference_of_squares(number):
+    return square_of_sum(number) - sum_of_squares(number)
+    # pass
